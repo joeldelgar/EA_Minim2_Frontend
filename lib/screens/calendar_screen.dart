538ddd9ed79b0aa1f232f806faf0c4dcd43bbd89
Joel Delgado Garrido <joel.delgado@estudiantat.upc.edu>
2022-06-07 12:13:51 +0200

    ContainercommitToni

--- lib/screens/calendar_screen.dart
+++ lib/screens/calendar_screen.dart
@@ -10,13 +10,82 @@ class CalendarPage extends StatefulWidget {
 }
 
 class _CalendarPageState extends State<CalendarPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SfCalendar(
-        view: CalendarView.month,
-      ),
-      
-    );
+@override
+Widget build(BuildContext context) {
+  return MaterialApp(
+    home: Scaffold(
+      body: Container(
+            child: SfCalendar(
+              view: CalendarView.month,
+              todayHighlightColor: Colors.red,
+              showNavigationArrow: true,
+              cellEndPadding: 5,
+              dataSource: MeetingDataSource(_getDataSource()),
+              monthViewSettings: MonthViewSettings(
+                appointmentDisplayMode: MonthAppointmentDisplayMode.appointment),
+              ),
+            ),
+          ),
+      );
   }
+}
+
+List<Meeting> _getDataSource(){
+  final List <Meeting> meetings = <Meeting>[];
+  final DateTime today = DateTime.now();
+  final DateTime startTime = DateTime(today.year, today.month, today.day, 9, 0, 0);
+  final DateTime endTime = startTime.add(const Duration(hours: 2));
+  meetings.add(Meeting('Time To Talk in English', startTime, endTime, const Color(0xFF0F8644), false));
+  return meetings;
+}
+
+class MeetingDataSource extends CalendarDataSource{
+  MeetingDataSource(List<Meeting> source){
+    appointments=source;
+  }
+ 
+@override
+  DateTime getStartTime(int index) {
+    return _getMeetingData(index).from;
+  }
+
+  @override
+  DateTime getEndTime(int index) {
+    return _getMeetingData(index).to;
+  }
+
+  @override
+  String getSubject(int index) {
+    return _getMeetingData(index).eventName;
+  }
+
+  @override
+  Color getColor(int index) {
+    return _getMeetingData(index).background;
+  }
+
+  @override
+  bool isAllDay(int index) {
+    return _getMeetingData(index).isAllDay;
+  }
+
+  Meeting _getMeetingData(int index){
+  final dynamic meeting = appointments![index];
+  late final Meeting meetingData;
+  if(meeting is Meeting){
+    meetingData = meeting;
+  }
+  return meetingData;
+}
+}
+
+
+
+class Meeting{
+  Meeting(this.eventName, this.from, this.to, this.background, this.isAllDay);
+  String eventName;
+  DateTime from;
+  DateTime to;
+  Color background;
+  bool isAllDay;
 }
